cf5b1dba4c6a7a2e104152ee5bb90ab37aa54cf1
Afik Ben Shimol <afik@nova.cs.tau.ac.il>
2023-05-12 10:03:21 +0300

    read fille and save to str split str to lines

--- kmeans.c
+++ kmeans.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-int EPS = 0.001;
-int ITER = 200;
+const int EPS = 0.001;
+const int ITER = 200;
+const char *GENERAL_ERROR = "An Error Has Occurred\n";
 
 typedef struct 
 {
@@ -44,7 +45,7 @@ int read_point(char *line, Point *point) {
 int get_dimension_num(FILE* file) {
     FILE* file = fopen("file.txt", "r");
     if (file == NULL) {
-        printf("Failed to open file\n");
+        printf(GENERAL_ERROR);
         return 1;
     }
 
@@ -58,6 +59,73 @@ int get_dimension_num(FILE* file) {
 
     fclose(file);
     return num_commas + 1;
+
+}
+
+// reads the string and counts dimentions
+int get_dimention(char *st){
+    int dim = 0;
+    for (int i = 0; (st[i] != '\n') && (st[i] != '\0'); i++){
+        dim += (st[i] == ',');
+    }
+    // add one if the first char is a number
+    dim += st[0] <= '9' && st[0] >='0'; 
+    return dim;
+}
+
+// counts the number of lines in the string
+int count_lines(char *st){
+    int line_cnt = 0;
+    for (int i = 0; st[i] != '\0'; i++){
+        line_cnt += (st[i] == '\n');
+    }
+    return line_cnt;
+}
+
+// reads the file and returns a pointer to the string
+char* read_file(FILE *file){
+    char *buffer;
+    long size;
+    FILE *file;
+    file = fopen("file.txt", "r");
+
+    if (file == NULL) {
+        printf(*GENERAL_ERROR);
+        return 0;
+    }
+
+    fseek(file, OL, SEEK_END); // go to end of file
+    size = ftell(file);   // save the size
+    fseek(file, OL, SEEK_SET); // go to start of file
+
+    // allocate memory
+    buffer = (char*)malloc(size+1)
+    if (*buffer == NULL){
+        printf(*GENERAL_ERROR);
+        return 0;
+    }
+    // read and save to buffer
+    fread(buffer, size, 1, file);
+    buffer[size] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+// take the string and split it to lines array
+char** split_to_lines(char *st, int line_cnt){
+    char **lines_arr;
+    lines_arr = calloc(lines_cnt, sizeof(char *));
+    
+    lines_arr[0] = &st[0]; // set to first line
+    for (int i = 0, j = 1; st[i] != '\0'; i++){
+        if (st[i] == '\n'){
+            st[i] = '\0'; // split the string
+            lines_arr[j] = &st[i+1]; // point to start of next line
+            j++;
+        }
+    }
+    return lines_arr;
 }
 
 int main(int argc, char *argv[])
@@ -75,7 +143,6 @@ int main(int argc, char *argv[])
     char *line = NULL;
     size_t len = 0;
     size_t read;
-
     FILE* file = fopen("file.txt", "r");
     int num_of_deimensions = get_dimension_num(file);
 
