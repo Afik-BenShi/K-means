b25c0ad8795cdaf3a1385f3ce57d4b3fb2a351ac
dor <dorlib318@gmail.com>
2023-05-15 13:55:44 +0300

    c_impl

--- kmeans.c
+++ kmeans.c
@@ -5,138 +5,117 @@
 const double EPS = 0.001;
 const int ITER = 200;
 const char* GENERAL_ERROR = "An Error Has Occurred\n";
+int dim = 0;
+int line_num = 0;
 
-typedef struct {
-    double *coords;
-    int dimension;
-    struct Point *next;
-} Point;
+/* ============ Array Tools ============ */
 
-Point *create_point(int dimension)
-{
-    Point *p = malloc(sizeof(Point));
-    if (p == NULL) {
-        printf("Error: Failed to allocate memory for Point\n");
-        return NULL;
+/*  frees 2d arrays. Array needs to be cast into void** */
+void free_2d(void** mat, int rows){
+    int i;
+    for (i = 0; i < rows; i++){
+        free(mat[i]);
     }
-    
-    p->dimension = dimension;
-    p->coords = malloc(dimension * sizeof(double));
-    if (p->coords == NULL) {
-        printf("Error: Failed to allocate memory for Point coordinates\n");
-        free(p);
-        return NULL;
+    free(mat);
+}
+/* Checks that an allocated pointer actually has somwere to point to */
+void pointer_check(void* ptr, const char* error_msg){
+    if (ptr == NULL) {
+        printf("%s", error_msg);
+        exit(-1);
     }
-    
-    // Initialize all coordinates to zero
-    for (int i = 0; i < dimension; i++) {
-        p->coords[i] = 0.0;
-    }
-    
-    return p;
+    return;
 }
 
-void destroy_point(Point *p)
-{
-    free(p->coords);
-    p->dimension = 0;
-}
+/* Creates an array of points that are all 0.
+ * len - length of the array
+ * Checks new pointers  */
+double** empty_points_arr(int len){
+    int i, j;
+    double **arr = (double **)malloc(len * sizeof(double *));
+    pointer_check((void *)arr, GENERAL_ERROR);
 
-typedef struct Cluster {
-    Point *members;
-    Point center;
-} Cluster;
-
-int read_point(char *line, Point *point) {
-    int i = 0;
-    int j = 0;
-    char num_str[256];
-    double num_val;
-
-    while (line[j] != '\0' && i < point->dimension) {
-        int k = 0;
-        while (line[j] != ',' && line[j] != '\0') {
-            num_str[k++] = line[j++];
-        }
-        num_str[k] = '\0';
-        num_val = 0.0;
-        int decimal_place = 0;
-        int is_negative = 0;
-
-        for (int l = 0; l < k; l++) {
-            if (num_str[l] == '-') {
-                is_negative = 1;
-            } else if (num_str[l] == '.') {
-                decimal_place = k - l - 1;
-            } else {
-                num_val = num_val * 10 + (num_str[l] - '0');
-            }
-        }
-
-        if (is_negative) {
-            num_val = -num_val;
-        }
-
-        num_val /= pow(10, decimal_place);
-        point->coords[i++] = num_val;
-
-        if (line[j] == ',') {
-            j++;
+    for (i = 0; i < len; i++){
+        arr[i] = (double *)malloc(dim * sizeof(double));
+        pointer_check((void *)arr[i], GENERAL_ERROR);
+        
+        for (j = 0; j < dim; j++){
+            arr[i][j] = 0.0;
         }
     }
-
-    return (i == point->dimension);
+    return arr;
 }
 
+/* Copies the first k points from one array to a new one.
+ * Checks new pointers */
+double** point_array_copy(double** points, int k){
+    int i, j;
+    double **new_points = (double **)malloc(k * sizeof(double *));
+    pointer_check((void *)new_points, GENERAL_ERROR);
 
-// reads the string and counts dimentions
+    for (i = 0; i < k; i++){
+        new_points[i] = (double *)malloc(dim * sizeof(double));
+        pointer_check((void *)new_points[i], GENERAL_ERROR);
+        /* copy the values directly and not by pointing */
+        for (j = 0; j < dim; j++){
+            new_points[i][j] = points[i][j];
+        }
+    }
+    return new_points;
+}
+
+/* ============ Global Variables Assignment ============*/
+
+/* Counts the dimention of the points 
+ * and updates global variable dim */
 int get_dimention(char *st){
-    int dim = 0;
-    for (int i = 0; (st[i] != '\n') && (st[i] != '\0'); i++){
-        dim += (st[i] == ',');
+    int i;
+    int dim_ = 0;
+    for (i = 0; (st[i] != '\n') && (st[i] != '\0'); i++){
+        /* count when seeing a comma */
+        dim_ += (st[i] == ',');
     }
 
-    // add one if the first char is a number
-    dim += st[0] <= '9' && st[0] >='0'; 
-    return dim + 1;
+    /* add one if the first char is a number
+     * this distinguishes if line has a number or is empty */
+    dim_ += (st[0] <= '9' && st[0] >='0') || st[0] == '-'; 
+    dim = dim_;
+    return dim_;
 }
 
-// counts the number of lines in the string
+/* Counts the number of lines in the string
+ * and updates global variable lines_cnt */
 int count_lines(char *st){
-    if (st == NULL) {
-        printf("Error: null pointer passed to count_lines\n");
-        return 0;
-    }
-    
+    int i;
     int line_cnt = 0;
-    for (int i = 0; st[i] != '\0'; i++){
+    pointer_check((void *)st, "Error: null pointer passed to count_lines\n");
+    
+    for (i = 0; st[i] != '\0'; i++){
         line_cnt += (st[i] == '\n');
     }
-
+    
+    line_num = line_cnt;
     return line_cnt;
 }
 
-// reads the file and returns a pointer to the string
-char* read_file(FILE *file){
+/* ============ File Read and String Manipulations ============*/
+
+/* Reads the file and saves it to a string.
+ * Checks new pointers */
+char* read_file(){
     char *buffer;
     long size;
+    FILE *file = stdin;
+    pointer_check((void *)file, GENERAL_ERROR);
+    fseek(file, 0L, SEEK_END); /* go to end of file*/
+    size = ftell(file);   /* save the size*/
+    fseek(file, 0L, SEEK_SET); /* go to start of file*/
 
-    if (file == NULL) {
-        printf(GENERAL_ERROR);
-        return NULL;
-    }
-
-    fseek(file, 0L, SEEK_END); // go to end of file
-    size = ftell(file);   // save the size
-    fseek(file, 0L, SEEK_SET); // go to start of file
-
-    // allocate memory
+    /* allocate string to hold the file data */
     buffer = (char*)malloc(size+1);
-    if (buffer == NULL){
-        printf(GENERAL_ERROR);
-        return NULL;
-    }
-    // read and save to buffer
+    pointer_check((void *)buffer, GENERAL_ERROR);
+
+    /* read and save to buffer*/
     fread(buffer, size, 1, file);
     buffer[size] = '\0';
 
@@ -145,231 +124,207 @@ char* read_file(FILE *file){
     return buffer;
 }
 
-// take the string and split it to lines array
-char** split_to_lines(char *st, int line_cnt){
+/* Take the string and split it to lines array.
+ * Warning: This replaces any '\n' with a '\0' in the original string,
+ * as the data is not copied to a new location in memory.
+ * Checks new pointers */
+char** split_to_lines(char *st, int lines_count){
     char **lines_arr;
-    lines_arr = calloc(line_cnt, sizeof(char *));
-    
-    lines_arr[0] = &st[0]; // set to first line
-    for (int i = 0, j = 1; st[i] != '\0'; i++){
+    int i, j;
+    lines_arr = (char **) malloc(lines_count * sizeof(char *));
+    pointer_check((void *)lines_arr, GENERAL_ERROR);
+
+    lines_arr[0] = &st[0]; /* point to first line*/
+    for (i = 0, j = 1; st[i] != '\0'; i++){
         if (st[i] == '\n'){
-            st[i] = '\0'; // split the string
-            lines_arr[j] = &st[i+1]; // point to start of next line
+            st[i] = '\0'; /* split the string*/
+            lines_arr[j] = &st[i+1]; /* point to start of next line*/
             j++;
         }
     }
-    
     return lines_arr;
 }
 
-int distance(Point* p1,Point* p2){
-        // verify dimentions.
-        if (p1->dimension != p2->dimension) {
-            printf("p1 dim = %d\n", p1->dimension);
-            printf("p2 dim = %d\n", p2->dimension);
-
-            printf(GENERAL_ERROR);
-            return NULL;        
+/* Takes a single line, splits it to numbers and returns a double array representing a point.
+ * Warning: This splits the string while running and mends it back in the end.
+ * Checks new pointers */
+double* read_point(char *line, int dimention) {
+    int i;
+    double* point = (double *)malloc(dim * sizeof(double));
+    pointer_check((void *)point, GENERAL_ERROR);
+    for (i = 0; i < dimention; i++){
+        int char_cnt;
+        double number;
+        char curr;
+        /* count the number of chars until the next ',' or '\0' */
+        for (char_cnt = 0;; char_cnt++){
+            curr = line[char_cnt];
+            if (curr == ',' || curr == '\0'){
+                line[char_cnt] = '\0';       /* mark end of number */
+                number = strtod(line, NULL); /* convert */
+                line[char_cnt] = curr;       /* revert changes */
+                break;
+            }
         }
-
-        int sum = 0;
-        for (int i = 0; i < p1->dimension; i++) {
-            sum += (p1->coords[i] - p2->coords[i]) * (p1->coords[i] - p2->coords[i]);
+        if (curr != '\0'){
+            line = &line[char_cnt + 1];
         }
-        
-        return sqrt(sum);
-}
-
-int count_points(Point *p) {
-    int count = 0;
-    while (p != NULL) {
-        count++;
-        p = p->next;
+        point[i] = number;
     }
-    return count;
+    return point;
 }
 
-// returns eclidean Distance, between the updated centroid to the previous one. 
-double recalc_center(Cluster *cluster) {
-    double coords[cluster->center.dimension];
-    for (int i = 0; i < cluster->center.dimension; i++) {
-        double sum = 0;
-        for (Point *p = cluster->members; p != NULL; p = p->next) {
-            sum += p->coords[i];
+/* Takes an array of lines, splits them to numbers 
+ * and returns an array of points from the lines.
+ * Warning: This replaces any ',' with a '\0' in each of the lines.
+ * as the data is not copied to a new location in memory.
+ * Checks new pointers */
+double** lines_to_points(char** lines, int line_num, int dim){
+    int i;
+    double **points = (double **)malloc(line_num * sizeof(double *));
+    pointer_check((void *)points, GENERAL_ERROR);
+    
+    for (i = 0; i < line_num; i++){
+        char *line = lines[i];
+        points[i] = read_point(line, dim);
+    }
+    return points;
+}
+
+/* Euclidian distance between p and q.
+ * Formula d=sqrt(pow(p[0]-q[0], 2) + ... + pow(p[n]-q[n], 2)) */
+double distance(double* p, double* q, int dim){
+    int i;
+    double dist = 0.0;
+    for (i = 0; i < dim; i++){
+        dist += pow((p[i]-q[i]), 2);
+    }
+    return sqrt(dist);
+}
+
+/* Calculate k-means of a 2d array representing poinds */
+double** kmeans(double** points, int k, int iter, double eps){
+    int i,j,l;
+    /* init centroids to first k points */
+    double **centroids = point_array_copy(points, k);
+    for (i = 0; i < iter; i++){
+        double **new_cents = empty_points_arr(k);
+                /* members saves what center assigned to each point.
+                * point[i] is assigned to centroids[members[i]] */
+        int *members = (int *)calloc(line_num, sizeof(int));
+                /* mem_cnt counts the number of points assigned to a center */
+        int *mem_cnt = (int *)calloc(k, sizeof(int));
+        int converg_cnt = 0;
+                /* foreach point*/
+        for (j = 0; j < line_num; j++){
+            int center_idx = 0;
+            double min_dist = -1;
+            /* find closest cluster*/
+            for (l = 0; l < k; l++){
+                double dist = distance(points[j], centroids[l], dim);
+                if ((min_dist == -1)||(min_dist > dist)){
+                    center_idx = l;
+                    min_dist = dist;
+                }
+            }
+            members[j] = center_idx;  /* point j is member of center_idx*/
+            mem_cnt[center_idx] += 1; /* count member*/
         }
-        coords[i] = sum / count_points(cluster->members);
+        /*step 4*/
+        for (j = 0; j < line_num; j++){
+            /* foreach point find its assigned index */
+            int center_idx = members[j];
+            for (l = 0; l < dim; l++){
+                /* sum the point components normalized by member count (avg point) */
+                new_cents[center_idx][l] += points[j][l]/mem_cnt[center_idx];
+            }
+        }
+        /*step 5*/
+        for (j = 0; j < k; j++){
+            /* count unchanged distances */
+            if (distance(new_cents[j], centroids[j], dim) < eps){
+                converg_cnt++;
+            }
+            /* also, we won't need the old centers anymore */
+            free(centroids[j]);
+        }
+        /* free all helping memory */
+        free(centroids);
+        free(members);
+        free(mem_cnt);
+        /* save new centers */
+        centroids = new_cents;
+        if (converg_cnt == k){
+            /* convergence */
+            break;
+        }
     }
-
-    Point* new_center = create_point(cluster->center.dimension);
-    for (int i = 0; i < cluster->center.dimension; i++) {
-        new_center->coords[i] = coords[i];
-    }
-
-    double delta = distance(new_center, &(cluster->center));
-    destroy_point(&(cluster->center));
-    cluster->center = *new_center;
-    free(new_center);
-
-    return delta;
+    return centroids;
 }
 
-double add(Cluster *cluster, Point *p) {
-    Point *new_member = malloc(sizeof(Point));
-    new_member->coords = p->coords;
-    new_member->dimension = p->dimension;
-
-    // Add the new point to the beginning of the linked list
-    new_member->next = cluster->members;
-    cluster->members = new_member;
-
-    return recalc_center(cluster);
-}
-
-void print_point(Point *p) {
-    printf("(");
-    for (int i = 0; i < p->dimension - 1; i++) {
-        printf("%f, ", p->coords[i]);
-    }
-    printf("%f)", p->coords[p->dimension - 1]);
-}
-
-void print_clusters(Cluster* clusters, int num_clusters) {
-    for (int i = 0; i < num_clusters; i++) {
-        printf("Cluster %d:\n", i);
-        print_point(&clusters[i]);
-        printf("\n");
-    }
-}
-
-void clear_members(Cluster *cluster) {
-    Point *p = cluster->members;
-    while (p != NULL) {
-        Point *next = p->next;
-        destroy_point(p);
-        p = next;
-    }
-    cluster->members = NULL;
-}
-
+/*  Checks that number of clusters is less than number of datapoints */
 void check_num_of_clusters(int num_of_clusters, int num_of_datapoints) {
     if (num_of_clusters <= 1 || num_of_clusters >= num_of_datapoints) {
         printf("Error: Invalid number of clusters!\n");
+        exit(-1);
     }
 }
-
-void check_num_of_iter(num_of_iter){
+/*  Checks that maximal number of iteration is within [1,1000] */
+void check_num_of_iter(int num_of_iter){
     if (num_of_iter <= 1 || num_of_iter >= 1000) {
         printf("Error: Invalid maximum iteration!\n");
+        exit(-1);
     }
 }
-
-Cluster* kmeans(Point* points, int num_points, int K, int iter, double eps) {
-    Cluster* clusters = (Cluster*) malloc(K * sizeof(Cluster));
-
-    // initialize clusters with random points
-    for (int i = 0; i < K; i++) {
-        clusters[i].center = points[i];
-        clusters[i].members = &points[i];
+int parse_k(int argc, char *argv[]){
+    int k;
+    /* parse and assert imput */
+    if (argc < 2){
+        printf("%s", GENERAL_ERROR);
+        return -1;
     }
-
-    for (int i = 0; i < iter; i++) {
-        int unchanged_clusters = 0;
-
-        // assign points to nearest cluster
-        for (int j = 0; j < num_points; j++) {
-            Point p = points[j];
-            Cluster* min_cluster = &clusters[0];
-            double min_dist = distance(&p, &min_cluster->center);
-                printf("in kmeans mindist dim = %d", min_cluster->center.dimension);
-
-
-            for (int k = 1; k < K; k++) {
-                double curr_dist = distance(&p, &clusters[k].center);
-                if (curr_dist < min_dist) {
-                    min_dist = curr_dist;
-                    min_cluster = &clusters[k];
-                }
-            }
-
-            if (add(min_cluster, &p) < eps) {
-                unchanged_clusters++;
-            }
-        }
-
-        if (unchanged_clusters == K) {
-            break;
-        }
-
-        // reset clusters so there aren't points in multiple clusters
-        for (int k = 0; k < K; k++) {
-            clear_members(&clusters[k]);
-        }
-    }
-
-    return clusters;
+    k = atoi(argv[1]);
+    return k;
 }
 
-int main(int argc, char *argv[])
-{
-    // Parse command line arguments
-    int k = atoi(argv[1]);
-    int max_iter = atoi(argv[2]);
-    check_num_of_iter(max_iter);
+int parse_iter(int argc, char *argv[]){
+    int iter = ITER;
+    if (argc >= 3){
+        iter = atoi(argv[2]);
+    }
+    return iter;
+}
 
-    char *line = NULL;
-    size_t len = 0;
-    size_t read;
+int main(int argc, char *argv[]){
+    int i, j; 
+    int k = parse_k(argc, argv);
+    int iter = parse_iter(argc, argv);
+    double eps = EPS;
+    char *text = read_file();
+    int dim_ = get_dimention(text);
+    int line_num_ = count_lines(text);
+    char **lines = split_to_lines(text, line_num_);
+    double **points = lines_to_points(lines, line_num_, dim_);
+    double **centroids;
     
-    int lines_counter = 0;
-    while ((read = getline(&line, &len, stdin)) != -1) {
-        lines_counter++;
-    }
-
-    fseek(stdin, 0, SEEK_SET);
-
-    Point **points = malloc(lines_counter * sizeof(Point*));
-
-    // Read the first line to count the number of dimensions
-    if ((read = getline(&line, &len, stdin)) != -1) {
-        int num_of_dimensions = get_dimention(line);
-        printf("Number of dimensions: %d\n", num_of_dimensions);
-
-        // Create a point to store the values read from each line
-        Point *point = create_point(num_of_dimensions);
-
-        fseek(stdin, 0, SEEK_SET);
-
-        int point_index = 0;
-
-        // Process the remaining input lines
-        while ((read = getline(&line, &len, stdin)) != -1) {
-            Point *point = create_point(num_of_dimensions);
-            if (read_point(line, point)) {
-                points[point_index++] = point;
-                printf("line = %s", line);
-                // Do something with the point
-                for (int i = 0; i < point->dimension; i++) {
-                    printf("%f ", point->coords[i]);
-                }
-                printf("\n");
-            } else {
-                printf("Failed to read point\n");
-                destroy_point(point);
-            }
+    /* free text variable*/
+    free(text);
+    free(lines);
+    /* check validity of k and iter */
+    check_num_of_clusters(k, line_num);
+    check_num_of_iter(iter);
+    centroids = kmeans(points, k, iter, eps);
+    for (i = 0; i < k; i++)
+    {
+        for (j = 0; j < dim-1; j++)
+        {
+            printf("%.4f,",centroids[i][j]);
         }
-
-        printf("points = %f\n", *points[1]);
-
-        destroy_point(point);
-    } else {
-        printf("Failed to read first line\n");
+        printf("%.4f\n",centroids[i][dim-1]);
     }
-
-    free(line);
-
-    check_num_of_clusters(k,lines_counter);
-    Cluster * clusters = kmeans(points, lines_counter, k, max_iter, EPS);
-    print_clusters(clusters, k);
-
-    return 0;
+    free_2d((void **)centroids, k);
+    free_2d((void **)points, line_num);
+    return 200;
 }
+
+
